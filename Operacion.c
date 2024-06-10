56de0d55085cca500e33cfab8164cb51dc108744
valeriaroman16 <a01662483@tec.mx>
2024-06-09 21:10:49 -0600

    Funciones añadidas

---
+++ Operacion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int suma(int a, int b){
+return a+b;
+}
+int resta(int a, int b){
+return a-b;
+}
+
+12
+int main(){
+int num1 = 12;
+int num2 = 3;
+int op1 = suma(num1,num2);
+printf("%i",op1);
+int op2 = resta(num1,num2);
+printf("%i",op2);
+
+return 0;
+}
+
